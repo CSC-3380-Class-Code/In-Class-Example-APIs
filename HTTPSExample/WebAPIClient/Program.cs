12c9acd2560fffcbbcb8a700b7856cd09c53a0a2
John Luke <jldenny0207@aol.com>
2024-10-29 01:00:00 -0500

    Added other api calls

--- HTTPSExample/WebAPIClient/Program.cs
+++ HTTPSExample/WebAPIClient/Program.cs
@@ -7,23 +7,26 @@ client.DefaultRequestHeaders.Accept.Add(
     new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
 client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
 
-var repositories = await ProcessRepositoriesAsync(client);
+string[] apis = ["https://api.github.com/orgs/dotnet/repos", "https://api.github.com/orgs/CSC-3380-Class-Code/repos"];
 
-foreach (var repo in repositories)
-{
-    Console.WriteLine($"Name: {repo.Name}");
-    Console.WriteLine($"Homepage: {repo.Homepage}");
-    Console.WriteLine($"GitHub: {repo.GitHubHomeUrl}");
-    Console.WriteLine($"Description: {repo.Description}");
-    Console.WriteLine($"Watchers: {repo.Watchers:#,0}");
-    Console.WriteLine($"{repo.LastPush}");
-    Console.WriteLine();
+foreach(string api in apis){
+    var repositories = await ProcessRepositoriesAsync(client, api);
+
+    foreach (var repo in repositories)
+    {
+        Console.WriteLine($"Name: {repo.Name}");
+        Console.WriteLine($"Homepage: {repo.Homepage}");
+        Console.WriteLine($"GitHub: {repo.GitHubHomeUrl}");
+        Console.WriteLine($"Description: {repo.Description}");
+        Console.WriteLine($"Watchers: {repo.Watchers:#,0}");
+        Console.WriteLine($"{repo.LastPush}");
+        Console.WriteLine();
+    }
 }
-
-static async Task<List<Repository>> ProcessRepositoriesAsync(HttpClient client)
+static async Task<List<Repository>> ProcessRepositoriesAsync(HttpClient client, string api)
 {
     await using Stream stream =
-        await client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+        await client.GetStreamAsync(api);
     var repositories =
         await JsonSerializer.DeserializeAsync<List<Repository>>(stream);
     return repositories ?? new();
